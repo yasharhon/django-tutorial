d272f2aa97229e90480e2aabdab92637bfbeb021
Yashar Honarmandi <yashar.honarmandi@entecon.se>
2023-09-03 00:44:33 +0200

    Add question-choice models and update with new views

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-%m$iin=y4ed5n7pkotc7ns=r5eavp%qmcz7ar!v_6yr399i0%9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.0.58",
+                 "localhost"]
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "polls.apps.PollsConfig"
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Stockholm'
 
 USE_I18N = True
 
